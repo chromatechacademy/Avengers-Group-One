4a5216218c0e19904349b76d097564baae75b1c1
EasyBusinessWay <easybusinessway2020@gmail.com>
2022-11-29 22:26:30 -0600

    CTSMS - Valid Credentials Login

---
+++ src/test/java/com/chroma/pages/LoginPage.java
@@ -0,0 +1,28 @@
+package com.chroma.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.chroma.web.WebDriverUtils;
+
+public class LoginPage {
+
+    /* Username Text Box */
+    @FindBy(xpath = "//input[@placeholder='Username']")
+    public WebElement userNameTextBox;
+
+    /* Password Text Box */
+    @FindBy(xpath = "//input[@placeholder='Password']")
+    public WebElement passwordTextBox;
+
+    /* SignIn button */
+    @FindBy(xpath = "//button[normalize-space()='Sign In']")
+    public WebElement signInButton;
+
+    public LoginPage() {
+
+        PageFactory.initElements(WebDriverUtils.driver, this);
+    }
+
+}
--- src/test/java/com/chroma/runners/RunCTSMSSmokeTest.java
+++ src/test/java/com/chroma/runners/RunCTSMSSmokeTest.java
@@ -1,7 +1,6 @@
 package com.chroma.runners;
 
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
@@ -11,12 +10,12 @@ import cucumber.api.junit.Cucumber;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"  }, 
 		features = "src/test/java/com/chroma/features", 
 		glue = "com.chroma.stepDefinitions", 
-		tags = "@Zhenya", 
+		tags = "", 
 		dryRun =  false, 
 		monochrome = true, 
 		strict = true
-) 
+)
 
-public class Runners {
+public class RunCTSMSSmokeTest {
     
 }
