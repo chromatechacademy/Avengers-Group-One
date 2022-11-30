48904fdef954ad0395a2c163e00d7bc5004ab409
reshad88 <reshad.alam@yahoo.com>
2022-11-30 00:20:30 -0500

    created featurefile

--- src/test/java/com/chroma/runners/Runners.java
+++ src/test/java/com/chroma/runners/Runners.java
@@ -11,8 +11,8 @@ import cucumber.api.junit.Cucumber;
 		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"  }, 
 		features = "src/test/java/com/chroma/features", 
 		glue = "com.chroma.stepDefinitions", 
-		tags = "@Practice", 
-		dryRun =  false, 
+		tags = "@AGO-44", 
+		dryRun =  true, 
 		monochrome = true, 
 		strict = true
 ) 
--- src/test/java/com/chroma/stepDefinitions/SampleStepDefinitions.java
+++ src/test/java/com/chroma/stepDefinitions/SampleStepDefinitions.java
@@ -2,10 +2,17 @@ package com.chroma.stepDefinitions;
 
 import com.chroma.appsCommon.PageInitializer;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class SampleStepDefinitions extends PageInitializer{
 
     // Add step definitions in this class
     // NOTE: Every step definitions class Extends PageInitializer
     // PageInitializer extends WebDriverUtils
+
+
+
     
 }
