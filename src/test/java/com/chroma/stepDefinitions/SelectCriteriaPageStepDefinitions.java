c781b40076752d1a0fe3d8332aa7ccf08d09cb34
Rukshana Akthar <rukshanaakthar@Rukshanas-MBP.home>
2022-12-02 16:52:49 -0500

    resolved conflicts

--- src/test/java/com/chroma/stepDefinitions/SelectCriteriaPageStepDefinitions.java
+++ src/test/java/com/chroma/stepDefinitions/SelectCriteriaPageStepDefinitions.java
@@ -1,6 +1,6 @@
 package com.chroma.stepDefinitions;
 
-import com.chroma.pages.DashboardPage;
+
 
 import cucumber.api.java.en.When;
 
@@ -8,7 +8,7 @@ public class SelectCriteriaPageStepDefinitions {
     
     @When("user is directed to dashboard page")
 public void user_is_directed_to_dashboard_page() {
-    DashboardPage
+    
 }
 
 
